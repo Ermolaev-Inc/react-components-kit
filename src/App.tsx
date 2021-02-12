3a0a0d159678595d9d2e4c5f15f3b8d45e85d21b
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-02-12 21:41:53 +0500

    Delete extra files

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
 
 const App: React.FC = () => {
   return (
     <Switch>
       <Route path="/">
-        <Home />
+        <></>
       </Route>
     </Switch>
   );
--- src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { ComponentExample } from "../components/ComponentExample";
-
-export const Home: React.FC = () => {
-  return (
-    <ComponentExample text="React Project" />
-  );
-};
