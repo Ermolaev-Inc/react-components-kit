3667b29e175dc310528d5ac6e7d108a6f3c5ca4b
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-03-02 15:23:14 +0500

    Create SVGIcon type

--- src/stories/Buttons/Button.ts
+++ src/stories/Buttons/Button.ts
@@ -6,6 +6,12 @@ type UserProps = {
   [name: string]: any;
 }
 
+export type SVGIcon = {
+  svg?: string;
+  svgWidth?: string;
+  svgHeight?: string;
+}
+
 export interface ButtonStyles {
   fontFamily?: string;
   fontSize?: string;
@@ -21,7 +27,7 @@ export interface ButtonRoundStyles extends ButtonRectStyles {
   diameter: string;
 }
 
-export type StandartProps = ButtonTitle & UserProps;
+export type StandartProps = ButtonTitle & UserProps & SVGIcon;
 export type ButtonProps = ButtonStyles & StandartProps;
 export type ButtonRectProps = ButtonRectStyles & StandartProps;
 export type ButtonRoundProps = ButtonRoundStyles & StandartProps;
