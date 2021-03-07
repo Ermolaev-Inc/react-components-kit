2cc3aa0a758af46849fef098b71a3c1f49053633
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-03-08 01:17:48 +0500

    Create infrastructure for NavbarStandart

--- src/stories/Navbars/Navbar.ts
+++ src/stories/Navbars/Navbar.ts
@@ -3,9 +3,29 @@ import React from "react";
 import { UserProps } from "../Interfaces";
 
 export interface NavbarStyles {
-  justifyContent: string;
   backgroundColor: string;
 }
 
-export type NavbarSimpleProps = UserProps &
-  NavbarStyles & { children: React.ReactNode };
+export interface NavbarSimple extends NavbarStyles {
+  justifyContent: string;
+}
+
+export interface SVGIcon {
+  src: string;
+  width: string;
+  height: string;
+}
+
+export interface NavbarStandartButtons {
+  flexBasis: string;
+}
+
+export type NavBarProps = UserProps & { children: React.ReactNode };
+
+export type NavbarSimpleProps = NavBarProps & NavbarSimple;
+
+export type NavbarStandartProps = NavBarProps & NavbarStyles;
+
+export type NavbarStandartLinkProps = SVGIcon & UserProps;
+
+export type NavbarStandartButtonsProps = NavBarProps & NavbarStandartButtons;
--- src/stories/Navbars/NavbarStyles.ts
+++ src/stories/Navbars/NavbarStyles.ts
@@ -1,9 +1,22 @@
 import styled from "styled-components";
-import { NavbarStyles } from "./Navbar";
+import { NavbarSimple, NavbarStyles, NavbarStandartButtons } from "./Navbar";
 
-export const NavbarWrapper = styled.div<NavbarStyles>`
+export const NavbarWrapper = styled.div<NavbarSimple>`
   display: flex;
   align-items: center;
   justify-content: ${props => props.justifyContent};
   background-color: ${props => props.backgroundColor};
+`;
+
+export const NavbarStandartStyles = styled.div<NavbarStyles>`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: ${props => props.backgroundColor};
+`;
+
+export const NavbarStandartButtonsStyles = styled.div<NavbarStandartButtons>`
+  flex-basis: ${props => props.flexBasis};
+  display: flex;
+  justify-content: center;
 `;
