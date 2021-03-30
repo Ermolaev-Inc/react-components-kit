f6a1b469708ea0831bdb0da6af6c095d48f9d609
Ilya Ermolaev <Ermolaev.ID@yandex.ru>
2021-03-30 22:37:07 +0500

    Delete extra lines in NavbarSimpleElement

--- src/stories/Navbars/NavbarSimple/NavbarSimpleElement.tsx
+++ src/stories/Navbars/NavbarSimple/NavbarSimpleElement.tsx
@@ -1,24 +1,14 @@
 import React from "react";
-import styled from "styled-components";
 
+import { NavbarSimpleElementProps } from "../Navbar";
 import { NavbarSimpleElementTitle } from "../NavbarStyles";
 
-const Title = styled.span`
-  display: block;
-  font-family: Roboto,serif;
-  font-size: 18px;
-  font-weight: 300;
-  cursor: pointer;
-  padding: 15px 30px;
-  transition: 0.5s;
-  :hover {
-    opacity: 50%;
-  }
-`;
-
-export const NavbarSimpleElement = ({ title = "Element" }: { title?: string }) => {
+export const NavbarSimpleElement: React.FC<Partial<NavbarSimpleElementProps>> = ({
+  title = "Element",
+  ...props
+}) => {
   return (
-    <NavbarSimpleElementTitle>
+    <NavbarSimpleElementTitle {...props}>
       {title}
     </NavbarSimpleElementTitle>
   );
